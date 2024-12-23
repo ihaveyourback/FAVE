63eaee2c6d5be1cb7bfd1be90424db2122ed5930
kimbumsu-kbs <vmvm1211@gmail.com>
2024-12-23 11:05:54 +0900

    2024-12-23

--- src/main/java/com/yhkim/fave/mappers/BoardPostMapper.java
+++ src/main/java/com/yhkim/fave/mappers/BoardPostMapper.java
@@ -12,4 +12,25 @@ public interface BoardPostMapper {
     List<BoardPostEntity> selectPostsByUserEmail(@Param("userEmail") String userEmail, @Param("pageVo") PageVo pageVo);
 
     int countPostsByUserEmail(String userEmail);
+
+//    김범수
+
+    BoardPostEntity[] selectBoardPosts();
+
+    int selectBoardPostCount();
+
+    BoardPostEntity[] selectBoardPost(@Param(value = "limitCount") int limitCount,
+                                       @Param(value = "offsetCount") int offsetCount);
+
+    int selectBoardPostCountBySearch(@Param(value = "filter") String filter,
+                                     @Param(value = "keyword") String keyword);
+
+    BoardPostEntity[] selectBoardPostBySearch(@Param(value = "filter") String filter,
+                                               @Param(value = "keyword") String keyword,
+                                               @Param(value = "limitCount") int limitCount,
+                                               @Param(value = "offsetCount") int offsetCount);
+
+    BoardPostEntity selectBoardPostsByIndex(@Param("index") int index);
+
+    int updateBoardPost(BoardPostEntity board);
 }
